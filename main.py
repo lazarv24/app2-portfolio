46de00635cdd08a3bc51861ecdb2f6bd9ca9c68b
lazar <lazar.srb.v@gmail.com>
2023-12-10 00:17:16 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo_of_me.jpg', width=700)
+    st.image('images/photo_of_me.jpg', width=600)
 
 with col2:
     st.title('Lazar Voštić')
@@ -20,4 +20,10 @@ with col2:
     '''
 
     st.info(content)
+
+content2 = '''
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+'''
+
+st.write(content2)
 
