d675cc295e976ff1cd786eec1a9bfe8eddbc41cd
lazar <lazar.srb.v@gmail.com>
2023-12-09 23:50:47 +0100

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo_of_me.jpg', width=700)
+
+with col2:
+    st.title('Lazar Voštić')
+    content = '''
+    Hello, I am Lazar. I am a Python programmer. For me coding is great way to show my skills and passion
+    for programming and computers in general. I have solid experience with programming for a young guy like me in early
+    twenties, when I was first started using Python, I decided to take a SoftUni course for programming basics and
+    finished the course with maximum grade. After that I expanded my knowledge and experience on another level,
+    listening to Udemy courses and many others, along with hard work I implement every day when I code to become better
+    as much as I can. And for me there are no limits with that. Also have experience with making graphical user
+    interface apps and web apps, web frameworks such as Streamlit, Heroku, Django and web development in general.
+    '''
+
+    st.info(content)
+
