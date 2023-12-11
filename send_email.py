7238ecd743196406eaa6f11c7e87871a31a781ae
lazar <lazar.srb.v@gmail.com>
2023-12-11 23:37:22 +0100

    Send emails using the form

--- send_email.py
+++ send_email.py
@@ -1,20 +1,17 @@
 import smtplib, ssl
 
-host = 'smtp.gmail.com'
-port = 465
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = 'vostic.l24@gmail.com'
+    password = 'fpzlmdfhksguotcl'
+
+    receiver = 'vostic.l24@gmail.com'
+    context = ssl.create_default_context()
 
 
-username = 'vostic.l24@gmail.com'
-password = 'fpzlmdfhksguotcl'
-
-receiver = 'vostic.l24@gmail.com'
-context = ssl.create_default_context()
-
-message = '''\
-Subject: Hi!
-How are you?
-Bye!
-'''
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message )
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
